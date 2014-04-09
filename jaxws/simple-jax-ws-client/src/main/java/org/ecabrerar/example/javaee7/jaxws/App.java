1a4f1bb5b2979a661cede45fe948b81331120d5a
Eudris Cabrera Rodriguez <eudris@gmail.com>
2014-04-09 01:34:17 -0400

    Simple java web services examples

---
+++ jaxws/simple-jax-ws-client/src/main/java/org/ecabrerar/example/javaee7/jaxws/App.java
@@ -0,0 +1,13 @@
+package org.ecabrerar.example.javaee7.jaxws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
