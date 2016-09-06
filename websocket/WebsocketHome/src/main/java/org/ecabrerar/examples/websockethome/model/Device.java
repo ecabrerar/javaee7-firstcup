8a7767bef28e08aeb3b82a28c34319e207390325
ecabrerar <eudris@gmail.com>
2016-09-06 17:11:47 -0400

    WebsocketHome Tutorial

---
+++ websocket/WebsocketHome/src/main/java/org/ecabrerar/examples/websockethome/model/Device.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ecabrerar.examples.websockethome.model;
+
+/**
+ *
+ * @author ecabrerar
+ * @date Sep 6, 2016
+ */
+public class Device {
+
+    private int id;
+    private String name;
+    private String status;
+    private String type;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ websocket/WebsocketHome/src/main/java/org/ecabrerar/examples/websockethome/ws/DeviceWebSocketServer.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ecabrerar.examples.websockethome.ws;
+
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import org.ecabrerar.examples.websockethome.model.Device;
+
+/**
+ *
+ * @author ecabrerar
+ * @date Sep 6, 2016
+ */
+@ApplicationScoped
+@ServerEndpoint("/actions")
+public class DeviceWebSocketServer {
+
+    @Inject
+    private DeviceSessionHandler sessionHandler;
+
+    @OnOpen
+    public void open(Session session) {
+        sessionHandler.addSession(session);
+    }
+
+    @OnClose
+    public void close(Session session) {
+        sessionHandler.removeSession(session);
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+        Logger.getLogger(DeviceWebSocketServer.class.getName()).log(Level.SEVERE, null, error);
+    }
+
+    @OnMessage
+    public void handleMessage(String message, Session session) {
+        try (JsonReader reader = Json.createReader(new StringReader(message))) {
+            JsonObject jsonMessage = reader.readObject();
+
+            if ("add".equals(jsonMessage.getString("action"))) {
+                Device device = new Device();
+                device.setName(jsonMessage.getString("name"));
+                device.setDescription(jsonMessage.getString("description"));
+                device.setType(jsonMessage.getString("type"));
+                device.setStatus("Off");
+                sessionHandler.addDevice(device);
+            }
+
+            if ("remove".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                sessionHandler.removeDevice(id);
+            }
+
+            if ("toggle".equals(jsonMessage.getString("action"))) {
+                int id = (int) jsonMessage.getInt("id");
+                sessionHandler.toggleDevice(id);
+            }
+        }
+    }
+}
